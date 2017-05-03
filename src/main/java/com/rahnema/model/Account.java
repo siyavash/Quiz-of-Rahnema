4f5925a64816b3bc52c24a0edb2b30dd9a3e8c1e
siyavash <siyavash.najafzade@gmail.com>
2017-05-03 10:55:49 +0430

    Account model added

---
+++ src/main/java/com/rahnema/model/Account.java
@@ -0,0 +1,87 @@
+package com.rahnema.model;
+
+/**
+ * Created by siyavash on 5/3/2017.
+ */
+public class Account {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String androidID;
+
+    private String email;
+
+    private int phoneNumber;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAndroidID() {
+        return androidID;
+    }
+
+    public void setAndroidID(String androidID) {
+        this.androidID = androidID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
