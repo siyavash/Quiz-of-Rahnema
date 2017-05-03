73ebd0a44900c72efcb8d605704e2525de5bdaae
siyavash <siyavash.najafzade@gmail.com>
2017-05-03 12:45:14 +0430

    register added to user controller

--- src/main/java/com/rahnema/controller/AccountController.java
+++ src/main/java/com/rahnema/controller/AccountController.java
@@ -1,7 +1,32 @@
 package com.rahnema.controller;
 
+import com.rahnema.model.Account;
+import com.rahnema.model.AccountDetail;
+import com.rahnema.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by siyavash on 5/3/2017.
  */
+
+@Controller
+@RequestMapping(path = "/user")
 public class AccountController {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @PostMapping(path = "register")
+    public @ResponseBody
+    Account register(@RequestHeader String androidId) {
+        Account newAccount = new Account(androidId);
+        accountRepository.save(newAccount);
+        return newAccount;
+    }
 }
