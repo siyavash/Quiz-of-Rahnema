fcd38e9c6602a771fa945ce1723fbc8f84bf7d68
siyavash <siyavash.najafzade@gmail.com>
2017-05-03 11:24:02 +0430

    jpa annotation added to account detail

--- src/main/java/com/rahnema/model/AccountDetail.java
+++ src/main/java/com/rahnema/model/AccountDetail.java
@@ -1,22 +1,31 @@
 package com.rahnema.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
  * Created by doost on 5/3/2017.
  */
+
+@Entity
+@Table(name = "account_detail")
 public class AccountDetail implements Serializable {
 
     private Account account;
 
+    @Column(name = "account_coin")
     private Long coin;
 
+    @Column(name = "account_gem")
     private Long gem;
 
+    @Column(name = "account_xp")
     private Long xp;
 
+    @Column(name = "account_level")
     private int level;
 
+    @Id
     public Account getAccount() {
         return account;
     }
