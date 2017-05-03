257784e3d5de10ee6966400dc8e0a334337770a8
siyavash <siyavash.najafzade@gmail.com>
2017-05-03 12:20:26 +0430

    account repository created

---
+++ src/main/java/com/rahnema/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.rahnema.repository;
+
+import com.rahnema.model.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by siyavash on 5/3/2017.
+ */
+
+@Repository
+public interface AccountRepository extends CrudRepository <Account, Long> {
+}
