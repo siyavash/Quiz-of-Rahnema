f810ac6b1264dafef88bd1dca8df3f7aec1a89cd
Siyavash Najaf Zade <siyavash.najafzade@gmail.com>
2017-05-05 22:39:14 +0430

    get function implemented in match controller

--- src/main/java/com/rahnema/controller/MatchController.java
+++ src/main/java/com/rahnema/controller/MatchController.java
@@ -1,14 +1,16 @@
 package com.rahnema.controller;
 
+import com.rahnema.model.Account;
 import com.rahnema.model.MatchDetail;
+import com.rahnema.repository.AccountRepository;
 import com.rahnema.repository.MatchDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by siyavash on 5/5/2017.
@@ -21,11 +23,26 @@ public class MatchController {
     @Autowired
     private MatchDetailRepository matchDetailRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @PostMapping(path = "/add", produces = "application/json")
     public @ResponseBody
     ResponseEntity addMatch(@RequestBody MatchDetail matchDetail) {
         matchDetailRepository.save(matchDetail);
 
         return ResponseEntity.accepted().body(null);
+    }
+
+    @PostMapping(path = "/get")
+    public @ResponseBody
+    ResponseEntity getMatch(@RequestHeader String androidId) {
+        Account account = accountRepository.findByAndroidId(androidId);
+
+        if(account == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return ResponseEntity.ok().body(matchDetailRepository.findAll());
     }
 }
