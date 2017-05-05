4e1b3033b9d4ab89d38030b2b7674d491ce20cea
Siyavash Najaf Zade <siyavash.najafzade@gmail.com>
2017-05-05 22:24:23 +0430

    add match function implemented

--- src/main/java/com/rahnema/controller/MatchController.java
+++ src/main/java/com/rahnema/controller/MatchController.java
@@ -1,9 +1,14 @@
 package com.rahnema.controller;
 
+import com.rahnema.model.MatchDetail;
 import com.rahnema.repository.MatchDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by siyavash on 5/5/2017.
@@ -14,5 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MatchController {
 
     @Autowired
-    MatchDetailRepository matchDetailRepository;
+    private MatchDetailRepository matchDetailRepository;
+
+    @PostMapping(path = "/add", produces = "application/json")
+    public @ResponseBody
+    ResponseEntity addMatch(@RequestBody MatchDetail matchDetail) {
+        matchDetailRepository.save(matchDetail);
+
+        return ResponseEntity.accepted().body(null);
+    }
 }
