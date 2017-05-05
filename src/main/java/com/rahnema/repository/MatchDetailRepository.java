c7cf5eb0d8d9578f3ef99317b62760a66a87f10b
Siyavash Najaf Zade <siyavash.najafzade@gmail.com>
2017-05-05 23:23:01 +0430

    repository annotation added to match detail repository

--- src/main/java/com/rahnema/repository/MatchDetailRepository.java
+++ src/main/java/com/rahnema/repository/MatchDetailRepository.java
@@ -2,9 +2,12 @@ package com.rahnema.repository;
 
 import com.rahnema.model.MatchDetail;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by siyavash on 5/5/2017.
  */
+
+@Repository
 public interface MatchDetailRepository extends CrudRepository<MatchDetail, Long> {
 }
