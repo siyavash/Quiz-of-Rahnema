c1ddc29e614e0e16e769b57f9bf458e779a2ca7f
Siyavash Najaf Zade <siyavash.najafzade@gmail.com>
2017-05-05 21:48:54 +0430

    match detail repository created

---
+++ src/main/java/com/rahnema/repository/MatchDetailRepository.java
@@ -0,0 +1,10 @@
+package com.rahnema.repository;
+
+import com.rahnema.model.MatchDetail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by siyavash on 5/5/2017.
+ */
+public interface MatchDetailRepository extends CrudRepository<MatchDetail, Long> {
+}
