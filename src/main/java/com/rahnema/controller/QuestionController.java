d0500c18c46466eabb5b3d9f5863a583a93fd344
siyavash <siyavash.najafzade@gmail.com>
2017-05-06 14:46:10 +0430

    add function implemented for question controller

--- src/main/java/com/rahnema/controller/QuestionController.java
+++ src/main/java/com/rahnema/controller/QuestionController.java
@@ -1,10 +1,15 @@
 package com.rahnema.controller;
 
+import com.rahnema.model.Question;
 import com.rahnema.repository.AccountRepository;
 import com.rahnema.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by doost on 5/6/2017.
@@ -19,5 +24,12 @@ public class QuestionController {
 
     @Autowired
     private AccountRepository accountRepository;
-    
+
+    @PostMapping(path = "/add")
+    public @ResponseBody
+    ResponseEntity addQuestion(@RequestBody Question question) {
+        questionRepository.save(question);
+
+        return ResponseEntity.accepted().body(null);
+    }
 }
