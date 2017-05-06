f045d8d80c7fa00264f9332b7f7dafb0991b88e0
siyavash <siyavash.najafzade@gmail.com>
2017-05-06 14:49:11 +0430

    demo get function implemented for question controller

--- src/main/java/com/rahnema/controller/QuestionController.java
+++ src/main/java/com/rahnema/controller/QuestionController.java
@@ -1,15 +1,13 @@
 package com.rahnema.controller;
 
+import com.rahnema.model.Account;
 import com.rahnema.model.Question;
 import com.rahnema.repository.AccountRepository;
 import com.rahnema.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by doost on 5/6/2017.
@@ -31,5 +29,17 @@ public class QuestionController {
         questionRepository.save(question);
 
         return ResponseEntity.accepted().body(null);
+    }
+
+    @PostMapping(path = "/get")
+    public @ResponseBody
+    ResponseEntity getQuestion(@RequestHeader String androidId) {
+        Account account = accountRepository.findByAndroidId(androidId);
+
+        if(account == null) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return ResponseEntity.ok().body(questionRepository.findAll());
     }
 }
