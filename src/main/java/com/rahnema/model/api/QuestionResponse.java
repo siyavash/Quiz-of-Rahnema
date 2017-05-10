83f77d1e9eea987f5cbf6056fcd16d712b8a24aa
siyavash <siyavash.najafzade@gmail.com>
2017-05-10 12:22:05 +0430

    question response body created

---
+++ src/main/java/com/rahnema/model/api/QuestionResponse.java
@@ -0,0 +1,32 @@
+package com.rahnema.model.api;
+
+import com.rahnema.model.entity.Question;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by doost on 5/10/2017.
+ */
+public class QuestionResponse implements Serializable {
+
+    private List<Question> questions;
+
+    private List<Long> deleted;
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public List<Long> getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(List<Long> deleted) {
+        this.deleted = deleted;
+    }
+}
