52e3afec6aec2263117708058ce00627aa36eb2e
siyavash <siyavash.najafzade@gmail.com>
2017-05-10 18:11:35 +0430

    find by inventory ids added with annotated query

--- src/main/java/com/rahnema/repository/QuestionRepository.java
+++ src/main/java/com/rahnema/repository/QuestionRepository.java
@@ -1,8 +1,14 @@
 package com.rahnema.repository;
 
+import com.rahnema.model.entity.Account;
 import com.rahnema.model.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by siyavash on 5/5/2017.
@@ -10,4 +16,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    String sql = "select p from Question p where p.id in :ids";
+
+    @Query(sql)
+    List<Question> findByInventoryIds(@Param("ids") List<Long> iterable);
 }
