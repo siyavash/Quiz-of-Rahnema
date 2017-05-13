577f8f9a05187cb2a11ff4b38de8421d6ef589c0
siyavash <siyavash.najafzade@gmail.com>
2017-05-13 15:09:12 +0430

    creating security configuration class

---
+++ src/main/java/com/rahnema/configuration/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.rahnema.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Created by siyavash on 5/13/2017.
+ */
+
+@Configuration
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
+}
