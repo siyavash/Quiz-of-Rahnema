d414eab26920bd783fa7dcf000a02efcf58d2c3d
siyavash <siyavash.najafzade@gmail.com>
2017-05-13 15:11:33 +0430

    overriding configure method for security configuration class

--- src/main/java/com/rahnema/configuration/SecurityConfiguration.java
+++ src/main/java/com/rahnema/configuration/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.rahnema.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
@@ -9,4 +12,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 
 @Configuration
 public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/oauth/token").permitAll()
+                .antMatchers("/user/register").permitAll()
+                .and().authorizeRequests().anyRequest().authenticated();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService);
+//    }
 }
