755eaccb5504c28396e71c6e1993778d77397c30
siyavash <siyavash.najafzade@gmail.com>
2017-05-13 15:02:38 +0430

    overriding configure method with AuthorizationServerEndpointsConfigurer argument

--- src/main/java/com/rahnema/configuration/Oauth2Configuration.java
+++ src/main/java/com/rahnema/configuration/Oauth2Configuration.java
@@ -1,8 +1,15 @@
 package com.rahnema.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * Created by siyavash on 5/13/2017.
@@ -11,4 +18,21 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAuthorizationServer
 @Configuration
 public class Oauth2Configuration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(new JdbcTokenStore(dataSource))
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+
+    }
 }
