128e9659ff72bee8c757d2c6444016d071ea7a5a
siyavash <siyavash.najafzade@gmail.com>
2017-05-13 15:05:27 +0430

    overriding configure method with AuthorizationServerSecurityConfigurer argument

--- src/main/java/com/rahnema/configuration/Oauth2Configuration.java
+++ src/main/java/com/rahnema/configuration/Oauth2Configuration.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
@@ -49,5 +50,12 @@ public class Oauth2Configuration extends AuthorizationServerConfigurerAdapter {
 ////                .resourceIds(RESOURCE_ID)
 //                .secret("12345621");
         ;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("permitAll()");
+        //    .passwordEncoder(passwordEncoder);
+
     }
 }
