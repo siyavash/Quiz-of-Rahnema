eab7e47938142614a5f8fb91767c5b43685e945d
siyavash <siyavash.najafzade@gmail.com>
2017-05-15 20:15:59 +0430

    constructor method access changed to public

--- src/main/java/com/rahnema/exception/UnavailableAbstractException.java
+++ src/main/java/com/rahnema/exception/UnavailableAbstractException.java
@@ -4,7 +4,7 @@ package com.rahnema.exception;
  * Created by siyavash on 5/15/2017.
  */
 public abstract class UnavailableAbstractException extends Exception {
-    private UnavailableAbstractException(String error) {
+    public UnavailableAbstractException(String error) {
         super(error + " already exist.");
     }
 }
