3b5fecf81df90d47efafb382b147bde9902a5e46
siyavash <siyavash.najafzade@gmail.com>
2017-05-15 19:47:38 +0430

    creating option repository

---
+++ src/main/java/com/rahnema/repository/OptionRepository.java
@@ -0,0 +1,10 @@
+package com.rahnema.repository;
+
+import com.rahnema.model.entity.Option;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by doost on 5/15/2017.
+ */
+public interface OptionRepository extends CrudRepository<Option, Long> {
+}
