afb04277db8444e669c35bb26daf1d82a76e29ce
siyavash <siyavash.najafzade@gmail.com>
2017-05-17 12:31:23 +0430

    get question func throw username not found exception and give 80% new questions and 20% duplicate questions

--- src/main/java/com/rahnema/controller/QuestionController.java
+++ src/main/java/com/rahnema/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.rahnema.controller;
 
+import com.rahnema.exception.UsernameNotFoundException;
 import com.rahnema.model.entity.Account;
 import com.rahnema.model.entity.Option;
 import com.rahnema.model.entity.Question;
@@ -8,8 +9,14 @@ import com.rahnema.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.lang.Math.min;
 
 /**
  * Created by siyavash on 5/6/2017.
@@ -42,20 +49,47 @@ public class QuestionController {
 
     @PostMapping(path = "/get")
     public @ResponseBody
-    ResponseEntity getQuestions(@RequestHeader String androidId) {
-        Account account = accountRepository.findByAndroidId(androidId);
+    ResponseEntity getQuestions(@RequestHeader String androidId) throws UsernameNotFoundException {
+        UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = user.getUsername();
+
+        Account account = accountRepository.findByUsername(username);
 
         if(account == null) {
-            return ResponseEntity.badRequest().body(null);
+            throw new UsernameNotFoundException(username);
         }
 
-        String sql = "SELECT question.id FROM question WHERE question.id NOT IN " +
-                "(SELECT question_account.question_id FROM question_account WHERE question_account.account_id = ?)";
+        //TODO static number
+        int numberOfNewQuestions = 10;
 
-        return ResponseEntity
-                .ok()
-                .body(questionRepository
-                    .findByInventoryIds(jdbcTemplate
-                        .query(sql, new Object[] {account.getId()}, (rs, rowNum) -> (rs.getLong("id")))));
+        String queryForNewQuestions =
+                "SELECT question.id " +
+                "FROM question " +
+                "WHERE question.id NOT IN (" +
+                    "SELECT question_account.question_id " +
+                    "FROM question_account " +
+                    "WHERE question_account.account_id = ?) " +
+                "ORDER BY rand() " +
+                "LIMIT ?";
+
+        List<Long> newQuestionIds = jdbcTemplate.query(queryForNewQuestions,
+                                            new Object[] {account.getId(), numberOfNewQuestions},
+                                            (rs, rowNum) -> (rs.getLong("id")));
+        int numberOfRemainQuestions = numberOfNewQuestions - (int) min(0.8 * numberOfNewQuestions, newQuestionIds.size());
+
+        String queryForDuplicateQuestions =
+                "SELECT question_account.question_id " +
+                "FROM question_account " +
+                "WHERE account_id = ? " +
+                "GROUP BY question_id " +
+                "ORDER BY rand() " +
+                "LIMIT ?";
+
+        List<Long> remainQuestions = jdbcTemplate.query(queryForDuplicateQuestions,
+                                                        new Object[] {account.getId(), numberOfRemainQuestions},
+                                                        (rs, rowNum) -> (rs.getLong("id")));
+        newQuestionIds.addAll(remainQuestions);
+
+        return ResponseEntity.ok().body(questionRepository.findByInventoryIds(newQuestionIds));
     }
 }
