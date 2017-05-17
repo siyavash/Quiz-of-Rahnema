22f5b17d660f89fd0c0de6a3ab18824f6e8c0f85
siyavash <siyavash.najafzade@gmail.com>
2017-05-17 10:59:02 +0430

    polish

--- src/main/java/com/rahnema/exception/UsernameExistsException.java
+++ src/main/java/com/rahnema/exception/UsernameExistsException.java
@@ -1,7 +1,7 @@
 package com.rahnema.exception;
 
 /**
- * Created by doost on 5/15/2017.
+ * Created by siyavash on 5/15/2017.
  */
 public class UsernameExistsException extends UnavailableAbstractException {
     public UsernameExistsException(String error) {
