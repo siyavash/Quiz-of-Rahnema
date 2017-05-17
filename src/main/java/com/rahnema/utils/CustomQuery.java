c728d1dcc47dc7da9a51949080aa55690827cd07
siyavash <siyavash.najafzade@gmail.com>
2017-05-17 12:32:10 +0430

    final custom query class created

---
+++ src/main/java/com/rahnema/utils/CustomQuery.java
@@ -0,0 +1,26 @@
+package com.rahnema.utils;
+
+/**
+ * Created by siyavash on 5/17/2017.
+ */
+
+public final class CustomQuery {
+
+    public static final String getNewQuestions =
+            "SELECT question.id " +
+            "FROM question " +
+            "WHERE question.id NOT IN (" +
+                "SELECT question_account.question_id " +
+                "FROM question_account " +
+                "WHERE question_account.account_id = ?) " +
+            "ORDER BY rand() " +
+            "LIMIT ?";
+
+    public static final String getDuplicateQuesions =
+            "SELECT question_account.question_id " +
+            "FROM question_account " +
+            "WHERE account_id = ? " +
+            "GROUP BY question_id " +
+            "ORDER BY rand() " +
+            "LIMIT ?";
+}
