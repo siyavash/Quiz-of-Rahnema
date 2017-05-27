e811c04bf8587811d92c5fe51363bd4ff8108b69
siyavash <siyavash.najafzade@gmail.com>
2017-05-27 16:24:11 +0430

    game config file created

---
+++ src/main/java/com/rahnema/utils/GameConfig.java
@@ -0,0 +1,17 @@
+package com.rahnema.utils;
+
+/**
+ * Created by siyavash on 5/27/2017.
+ */
+public final class GameConfig {
+
+    public static final long baseCoin = 100;
+
+    public static final long baseGem = 0;
+
+    public static final long baseXp = 0;
+
+    public static final long baseLevel = 1;
+
+    public static final int numberOfFreshQuestions = 42;
+}
